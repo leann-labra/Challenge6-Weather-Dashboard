5aa319fb74bfb5d7cc0fee275246215efc6e77ab
leann-labra <leann.labra@gmail.com>
2022-10-24 17:29:30 -0700

    created structure for index.html and began pseudo code for js

---
+++ weather.js
@@ -0,0 +1,24 @@
+//-----------------PSUEDO CODE-------------------------//
+///read up on weather api documentation
+///dynamically create list elements for a table, 1 big card, and 5 smaller cards
+///Left sidebar:
+    //input to search for city
+    //button to store user information 
+    //trigger function to search for city 
+    //list of cities underneath search bar
+
+//top right card: 
+    //Name of city rendered
+    //li/p elements rendered
+    //temperature
+    //wind
+    //humidity
+    //UV index-- UV index number in green square card. Bg-green
+
+//bottom right 5 cards: connected to moment? 
+    //current date
+    //icon that indicates forecast conditions for the day
+    //list of weather conditions
+
+//-------------------API KEY--------------------------//
+var weatherlyAPI = "647f561906a8e44debcd1c6086357775";
